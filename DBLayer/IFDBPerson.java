1e1a7d003a7e17dfadbaca0c79ad3a8b513be76b
mrStrainer <atee88@gmail.com>
2017-06-06 12:33:57 +0200

    some more interfaces

--- DBLayer/IFDBPerson.java
+++ DBLayer/IFDBPerson.java
@@ -1,7 +1,15 @@
 package DBLayer;
 
+import ModelLayer.Person;
+
+import java.util.ArrayList;
+
 /**
  * Created by atee on 2017.06.02..
  */
 public interface IFDBPerson {
+    public ArrayList<Person> getAllPersons(boolean retriveAssociation);
+    public Person findSupplier(int id, boolean retriveAssociation);
+    public Person findCustomer(int id, boolean retriveAssociation);
+    public int insertPerson(Person person) throws Exception;
 }
